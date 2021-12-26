707b9b51f325d1e3e83d6af347f113b8060ee533
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2021-12-26 21:04:21 +0100

    Create fxmanifest.lua

---
+++ ND_ShotSpotter/fxmanifest.lua
@@ -0,0 +1,14 @@
+-- For support join my discord: https://discord.gg/Z9Mxu72zZ6
+
+author "Andyyy#7666"
+description "Shot Spotter Script (ND Framework)"
+version "1.0"
+
+fx_version "cerulean"
+game "gta5"
+
+server_script "server.lua"
+client_scripts {
+    "config.lua",
+    "client.lua"
+}
