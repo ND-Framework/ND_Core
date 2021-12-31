a2adeb275876fe87b7cf582baf7d7554d0ac7ce3
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2021-12-31 02:51:34 +0100

    Update fxmanifest.lua

--- ND_Core/fxmanifest.lua
+++ ND_Core/fxmanifest.lua
@@ -2,7 +2,7 @@
 
 author "Andyyy#7666"
 description "ND Framework Core"
-version "1.0"
+version "1.1"
 
 fx_version "cerulean"
 game "gta5"
@@ -32,6 +32,25 @@ client_scripts {
 
 exports {
     "getCharacterInfo", -- getCharacterInfo(infoType), 1 will return return FirstName, 2 LastName, 3 DateOfBirth, 4 Gender, 5 TwtName, 6 Department, 7 Cash, 8 Bank. 9 will return their character id.
-    "sendBank", -- sendBank(sendingCharacterId, receiveingPlayerId, amount, sendingPlayerId)
-    "sendCash" -- sendCash(sendingCharacterId, amount, sendingPlayerId)
 }
+
+server_exports {
+    "getCharacterTable", -- exports["ND_Core"]:getCharacterTable() this will return a table with all the online players and their info. View below on how to use this.
+}
+
+--[[
+    Info:
+    characterId
+    firstName
+    lastName
+    dob
+    gender
+    twt
+    dept
+    cash
+    bank
+
+    Usage:
+    players = exports["ND_Core"]:getCharacterTable()
+    print(players[3].bank) this will print the bank account of the player with id 3.
+]]
