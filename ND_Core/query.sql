1b9003d578ce02805f36fba725ca1500e1b0a20d
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-02-26 01:32:55 +0100

    Update query.sql
    Added new default data so it can work if the money system is disabled.

--- ND_Core/query.sql
+++ ND_Core/query.sql
@@ -1,15 +1,12 @@
 CREATE TABLE `characters` (
-	`license` VARCHAR(200) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci',
-	`character_id` INT(50) NULL DEFAULT NULL,
-	`first_name` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	`last_name` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	`dob` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	`gender` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	`twt` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	`department` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	`cash` INT(11) NULL DEFAULT NULL,
-	`bank` INT(11) NULL DEFAULT NULL
-)
-COLLATE='utf8mb4_general_ci'
-ENGINE=InnoDB
-;
+	`license` VARCHAR(200) DEFAULT NULL,
+	`character_id` INT(10) DEFAULT NULL,
+	`first_name` VARCHAR(50) DEFAULT NULL,
+	`last_name` VARCHAR(50) DEFAULT NULL,
+	`dob` VARCHAR(50) DEFAULT NULL,
+	`gender` VARCHAR(50) DEFAULT NULL,
+	`twt` VARCHAR(50) DEFAULT NULL,
+	`department` VARCHAR(50) DEFAULT NULL,
+	`cash` INT(10) NULL DEFAULT 0,
+	`bank` INT(10) NULL DEFAULT 0
+);
