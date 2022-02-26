4f4e8435b68d8b0c8770ee37c39ee5cbacc52f4d
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-02-26 01:34:43 +0100

    Update config_server.lua
    Added SonoranCAD integration

--- ND_Core/config_server.lua
+++ ND_Core/config_server.lua
@@ -1,16 +1,10 @@
 -- For support join my discord: https://discord.gg/Z9Mxu72zZ6
 
 server_config = {
-    DiscordToken = "0", -- discord bot token for permissions
-    GuildId = "872496972454592523", -- discord guild id
+    DiscordToken = "0", -- discord bot token for permissions.
+    GuildId = "872496972454592523", -- discord guild id.
 
-    -- role id for each role of the department in your discord. 0 Gives the role to everyone.
-    roles = {
-        ["ADMIN"] = "0", -- You can remove this if you don't want it but this should only be here for permisisons not as a department and it won't display as a department. This can be used for /setaop and other permissions.
-        ["CIV"] = "0", 
-        ["SAHP"] = "0",
-        ["LSPD"] = "0",
-        ["BCSO"] = "0",
-        ["LSFD"] = "0"
-    }
+    SonoranCAD_Enabled = false, -- Turn this on if you want to use SonoranCAD integration.
+    SonoranCAD_CommunityID = "",
+    SonoranCAD_APIKey = ""
 }
