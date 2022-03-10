8310d778009c8801d608d4e270f1771738c88bbd
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-03-10 23:12:39 +0100

    Update server.lua

--- ND_Jailing/source/server.lua
+++ ND_Jailing/source/server.lua
@@ -10,19 +10,27 @@ function sendToDiscord(name, message, color)
             color = color
         }
     }
-    PerformHttpRequest(config.discordWebhook, function(err, text, headers) end, 'POST', json.encode({username = "ND Jailing", embeds = embed}), {["Content-Type"] = "application/json"})
+    PerformHttpRequest(server_config.discordWebhook, function(err, text, headers) end, 'POST', json.encode({username = "ND Jailing", embeds = embed}), {["Content-Type"] = "application/json"})
 end
 
 -- Jail player discord log, trigger the even on the players client and send a message to everyone.
 RegisterNetEvent("ND_Jailing:jailPlayer")
 AddEventHandler("ND_Jailing:jailPlayer", function(id, name, time, reason)
-    TriggerClientEvent("ND_Jailing:jailPlayer", id, time)
-    sendToDiscord("Jail Logs", "**" .. GetPlayerName(source) .. "** Jailed **" .. name .. "** for **" .. time .. " seconds** with the reason: **" .. reason .. "**.", 1595381)
-    TriggerClientEvent('chat:addMessage', -1, {
-        color = { 255, 0, 0},
-        multiline = true,
-        args = {"[Judge]", name .. " was charaged with " .. reason .. " and will be spending " .. time .. " months in jail."}
-    })
+    local player = source
+    local players = exports["ND_Core"]:getCharacterTable()
+    local dept = players[player].dept
+    for department in pairs(config.accessDepartments) do
+        if department == dept then
+            TriggerClientEvent("ND_Jailing:jailPlayer", id, time)
+            sendToDiscord("Jail Logs", "**" .. GetPlayerName(source) .. "** Jailed **" .. name .. "** for **" .. time .. " seconds** with the reason: **" .. reason .. "**.", 1595381)
+            TriggerClientEvent('chat:addMessage', -1, {
+                color = { 255, 0, 0},
+                multiline = true,
+                args = {"[Judge]", name .. " was charaged with " .. reason .. " and will be spending " .. time .. " months in jail."}
+            })
+            break
+        end
+    end
 end)
 
 -- Gets all players on the server and adds them to a table.
