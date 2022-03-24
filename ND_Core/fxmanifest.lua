a407bf6038f3c49080e8c556e99349658b56a2ad
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-03-24 21:28:33 +0100

    Update fxmanifest.lua

--- ND_Core/fxmanifest.lua
+++ ND_Core/fxmanifest.lua
@@ -2,7 +2,7 @@
 
 author "Andyyy#7666"
 description "ND Framework Core"
-version "1.2"
+version "1.3"
 
 fx_version "cerulean"
 game "gta5"
@@ -31,26 +31,22 @@ client_scripts {
 }
 
 exports {
-    "getCharacterInfo", -- exports["ND_Core"]:getCharacterInfo(infoType)
+    "getCharacterInfo", -- getCharacterInfo(infoType), 1 will return return FirstName, 2 LastName, 3 DateOfBirth, 4 Gender, 5 TwtName, 6 Department, 7 Cash, 8 Bank. 9 will return their character id.
 }
---[[
-    Info types:
-
-    1: First name
-    2: Last name
-    3: Date of birth
-    4: Gender
-    5: Twotter name
-    6: Department
-    7: Cash
-    8: Bank
-    9: character id
-]]
-
 server_exports {
-    "getCharacterTable", -- exports["ND_Core"]:getCharacterTable()
+    "transferBank", -- exports["ND_Core"]:transferBank(amount, player, target)
+    "giveCashToClosestTarget", -- exports["ND_Core"]:giveCashToClosestTarget(amount, player)
+    "withdrawMoney", -- exports["ND_Core"]:withdrawMoney(amount, player)
+    "depositMoney", -- exports["ND_Core"]:depositMoney(amount, player)
+    "deductMoney", -- exports["ND_Core"]:deductMoney(amount, player, from)
+    "addMoney", -- exports["ND_Core"]:addMoney(amount, player, to)
+
+    "getCharacterTable" -- exports["ND_Core"]:getCharacterTable() this will return a table with all the online players and their info. View below on how to use this.
 }
+
 --[[
+    Keys in the getCharacterTable:
+    
     characterId
     firstName
     lastName
@@ -61,7 +57,7 @@ server_exports {
     cash
     bank
 
-    Example:
+    Usage:
     players = exports["ND_Core"]:getCharacterTable()
-    print(players[3].bank) this will print the bank account balance of the player with id 3.
+    print(players[3].bank) this will print the bank account of the player with id 3.
 ]]
