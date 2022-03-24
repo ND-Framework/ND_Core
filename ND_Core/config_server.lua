66d7f4814f9d7f7f9aa81b05589d151cfe9f1639
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-03-24 21:28:51 +0100

    Update config_server.lua

--- ND_Core/config_server.lua
+++ ND_Core/config_server.lua
@@ -1,10 +1,12 @@
 -- For support join my discord: https://discord.gg/Z9Mxu72zZ6
 
 server_config = {
-    DiscordToken = "0", -- discord bot token for permissions.
-    GuildId = "872496972454592523", -- discord guild id.
+    DiscordToken = "0", -- discord bot token for permissions
+    GuildId = "872496972454592523", -- discord guild id
 
     SonoranCAD_Enabled = false, -- Turn this on if you want to use SonoranCAD integration.
     SonoranCAD_CommunityID = "",
-    SonoranCAD_APIKey = ""
+    SonoranCAD_APIKey = "",
+
+    useKVP = false
 }
