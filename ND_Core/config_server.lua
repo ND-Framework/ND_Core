44f3ad3adab37886d66fd22f1f41f1d4c9e2971d
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-04-08 19:26:43 +0200

    Update config_server.lua
    removed SonoranCAD API and now servers can use database sync instead.

--- ND_Core/config_server.lua
+++ ND_Core/config_server.lua
@@ -3,10 +3,4 @@
 server_config = {
     DiscordToken = "0", -- discord bot token for permissions
     GuildId = "872496972454592523", -- discord guild id
-
-    SonoranCAD_Enabled = false, -- Turn this on if you want to use SonoranCAD integration.
-    SonoranCAD_CommunityID = "",
-    SonoranCAD_APIKey = "",
-
-    useKVP = false
 }
