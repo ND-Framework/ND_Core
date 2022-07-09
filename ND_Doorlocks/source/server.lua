b84713e9a410430346083675c59deefb55a128cd
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-07-09 19:35:06 +0200

    Syncing doors when a new player joins.

--- ND_Doorlocks/source/server.lua
+++ ND_Doorlocks/source/server.lua
@@ -1,5 +1,14 @@
 -- For support join my discord: https://discord.gg/Z9Mxu72zZ6
+local updatedDoors = {}
+
 RegisterNetEvent("ND_Doorlocks:syncDoor")
 AddEventHandler("ND_Doorlocks:syncDoor", function(doorID, state)
+    updatedDoors[doorID] = state
     TriggerClientEvent("ND_Doorlocks:syncDoor", -1, doorID, state)
+end)
+
+RegisterNetEvent("ND_Doorlocks:getDoors")
+AddEventHandler("ND_Doorlocks:getDoors", function()
+    local player = source
+    TriggerClientEvent("ND_Doorlocks:returnDoors", player, updatedDoors)
 end)
