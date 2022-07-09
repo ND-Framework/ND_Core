604937921a0b4b4b8b63622a213bc10e394f3a95
HakkoDevelopment <67168651+Hakkodevelopment@users.noreply.github.com>
2022-07-09 16:23:37 -0600

    fixed grammar and spelling errors

--- ND_Jailing/config_client.lua
+++ ND_Jailing/config_client.lua
@@ -1,24 +1,24 @@
 config = {
-    maxJailTime = 300, -- Max time in seconds that someone can jail another player.
+    maxJailTime = 300, -- Max time in seconds that someone can jail another player for.
     jailDistance = 90.0, -- How far can the player go from the Jail coordinates. If the player is further than the set amount they will be teleported back.
 
     jailCoords = {x = 1680.21, y = 2513.25, z = 44.56, h = 6.53},
     releaseCoords = {x = 1840.22, y = 2608.42, z = 45.58, h = 270.09},
 
-    -- Where can players access it from.
+    -- Where can players access the UI from.
     accessLocation = {
         {x = -450.17, y = 6012.62, z = 31.71}, -- Paleto Bay Sheriff's Office
         {x = 1853.57, y = 3690.74, z = 34.26}, -- Sandy Shores Sheriff's Office
         {x = 460.53, y = -988.84, z = 24.91} -- Mission Row PD
     },
 
-    -- Departments that can access the jail tablet.
+    -- Departments that can access the jail UI.
     accessDepartments = {
         "SAHP",
         "LSPD",
         "BCSO"
     },
 
-    -- How close does the player have to be to the coordiantes to be able to open the tablet.
+    -- How close the player must be to access the jail UI
     accessDistance = 1.2,
 }
