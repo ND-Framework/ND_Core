2e1f1fe28efdb8a5f72e2df7dc9ce4f97a2cae36
Niko <54130154+nik0-3d@users.noreply.github.com>
2022-07-23 00:55:56 -0400

    lol

--- ND_Core/config_server.lua
+++ ND_Core/config_server.lua
@@ -1,7 +1,6 @@
 -- For support join my discord: https://discord.gg/Z9Mxu72zZ6
 
 server_config = {
-    -- discordServerToken = "OTAwODg1MDMxMDQ2Nzc0ODQ1.GU1TSC.Ed8D2_wcU-UQ1vZhe1khahgV8J3hLahOp4N1D8", -- discord bot token for permissions
-    discordServerToken = "OTAwODg1MDMxMDQ2Nzc0ODQ1.GE_e1I.pqEyV6GSfjs-0edjvvjtr8azyWDJG7hzUIPob8",
+    discordServerToken = "OTAwODg1MDMxMDQ2Nzc0ODQ1.GU1TSC.Ed8D2_wcU-UQ1vZhe1khahgV8J3hLahOp4N1D8", -- discord bot token for permissions
     guildId = "872496972454592523", -- discord guild id
 }
