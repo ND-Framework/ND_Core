db8c18995f60780ebe303ab96ee71ffa76d0fbe4
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-09-23 20:19:37 +0200

    fix: version checker

--- ND_ATMs/source/server.lua
+++ ND_ATMs/source/server.lua
@@ -1,5 +1,5 @@
 NDCore = exports["ND_Core"]:GetCoreObject()
-NDCore.Functions.VersionChecker("ND_ATMs", GetCurrentResourceName(), "https://github.com/Andyyy7666/ND_Framework", "https://raw.githubusercontent.com/Andyyy7666/ND_Framework/main/ND_ATMs/fxmanifest.lua")
+NDCore.Functions.VersionChecker("ND_ATMs", GetCurrentResourceName(), "https://github.com/ND-Framework/ND_Framework", "https://raw.githubusercontent.com/ND-Framework/ND_Framework/main/ND_ATMs/fxmanifest.lua")
 
 RegisterNetEvent("ND_ATMs:withdraw", function(amount)
     local player = source
