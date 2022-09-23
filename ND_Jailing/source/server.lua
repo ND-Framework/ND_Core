5ca7af9ab4a0e780b0fb3c29659de59c30e7ea1d
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-09-23 20:17:21 +0200

    fix: version checker

--- ND_Jailing/source/server.lua
+++ ND_Jailing/source/server.lua
@@ -1,5 +1,5 @@
 NDCore = exports["ND_Core"]:GetCoreObject()
-NDCore.Functions.VersionChecker("ND_Jailing", GetCurrentResourceName(), "https://github.com/Andyyy7666/ND_Framework", "https://raw.githubusercontent.com/Andyyy7666/ND_Framework/main/ND_Jailing/fxmanifest.lua")
+NDCore.Functions.VersionChecker("ND_Jailing", GetCurrentResourceName(), "https://github.com/ND-Framework/ND_Framework", "https://raw.githubusercontent.com/ND-Framework/ND_Framework/main/ND_Jailing/fxmanifest.lua")
 
 local jailedPlayers = {}
 
