a2ab94c226fbbfd7c53f03dd5f1602101bcd8d5a
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-09-24 21:41:01 +0200

    Delete ND_ShotSpotter directory

--- ND_ShotSpotter/config_server.lua
+++
@@ -1,4 +0,0 @@
-server_config = {
-    useDiscordLogging = true,
-	discordWebhook = "https://discord.com/api/webhooks/873744008122335252/zk53XWv0ZfD-BD2Su6VYGPzUgNW0NjLrj7hVBQFVgp_ByO0f0rpMwx7fsNNNjyVp4GUT",
-}
