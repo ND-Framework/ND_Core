d92d838633898f22cd8d20c947f0518bb993715a
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-10-04 00:30:59 +0200

    Update and rename ND_Core/client/main.lua to client/main.lua

--- client/main.lua
+++ client/main.lua
@@ -58,31 +58,6 @@ if config.customPauseMenu then
     end)
 end
 
-RegisterNetEvent("ND:returnCharacters", function(characters)
-    NDCore.Characters = characters
-end)
-
--- updates the money on the client.
-RegisterNetEvent("ND:updateMoney", function(cash, bank)
-    NDCore.SelectedCharacter.cash = cash
-    NDCore.SelectedCharacter.bank = bank
-end)
-
--- Sets main character.
-RegisterNetEvent("ND:setCharacter", function(character)
-    NDCore.SelectedCharacter = character
-end)
-
--- Update main character info.
-RegisterNetEvent("ND:updateCharacter", function(character)
-    NDCore.SelectedCharacter = character
-end)
-
--- Updates last lcoation.
-RegisterNetEvent("ND:updateLastLocation", function(location)
-    NDCore.SelectedCharacter.lastLocation = location
-end)
-
 -- Enables pvp if it's selected in the config.
 AddEventHandler("playerSpawned", function()
     if config.enablePVP then
