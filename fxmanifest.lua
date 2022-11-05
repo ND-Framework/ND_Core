d6c405e14ffe21b8403249551678b09372b35027
WLVF <65689785+WLVF@users.noreply.github.com>
2022-11-04 22:02:59 -0400

    New Command Loader Pog

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@
 
 author "Andyyy#7666, N1K0#0001"
 description "ND Framework Core"
-version "3.1.3"
+version "3.1.4"
 
 fx_version "cerulean"
 game "gta5"
@@ -17,6 +17,7 @@ server_scripts {
     "@oxmysql/lib/MySQL.lua",
     "config_server.lua",
     "server/main.lua",
+    "server/commands.lua",
     "server/functions.lua",
     "server/events.lua"
 }
