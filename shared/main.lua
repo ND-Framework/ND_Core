2fb940fe7e7658433c81d2544d7c37d0d09ceb07
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-11-30 03:31:51 +0100

    feat: resource check

---
+++ shared/main.lua
@@ -0,0 +1,25 @@
+local startedResources = {}
+local callbacks = {}
+
+AddEventHandler("onResourceStart", function(resourceName)
+    startedResources[resourceName] = true
+    local callback = callbacks[resourceName]
+    if not callback then return end
+    callback(true)
+end)
+
+AddEventHandler("onResourceStop", function(resourceName)
+    startedResources[resourceName] = nil
+    local callback = callbacks[resourceName]
+    if not callback then return end
+    callback(false)
+end)
+
+function isResourceStarted(resourceName, cb)
+    local started = GetResourceState(resourceName) == "started"
+    startedResources[resourceName] = started
+    callbacks[resourceName] = cb
+    return started
+end
+
+exports("isResourceStarted", isResourceStarted)
