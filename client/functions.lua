30ac1269497020baaf8246f001e3e1d6ce2c3034
Dark3581 <67527731+Dark3581@users.noreply.github.com>
2022-12-27 12:33:36 -0800

    GetPlayersFromCoords - Converted from qb/esx
    gets all players within an area

--- client/functions.lua
+++ client/functions.lua
@@ -10,6 +10,28 @@ function NDCore.Functions.GetCharacters()
     return NDCore.Characters
 end
 
+
+function NDCore.Functions.GetPlayersFromCoords(coords, distance)
+    local players = GetActivePlayers()
+    local ped = PlayerPedId()
+    if coords then
+        coords = type(coords) == 'table' and vec3(coords.x, coords.y, coords.z) or coords
+    else
+        coords = GetEntityCoords(ped)
+    end
+    distance = distance or 5
+    local closePlayers = {}
+    for _, player in pairs(players) do
+        local target = GetPlayerPed(player)
+        local targetCoords = GetEntityCoords(target)
+        local targetdistance = #(targetCoords - coords)
+        if targetdistance <= distance then
+            closePlayers[#closePlayers + 1] = player
+        end
+    end
+    return closePlayers
+end
+
 -- Callbacks are licensed under LGPL v3.0
 -- <https://github.com/overextended/ox_lib>
 NDCore.callback = {}
