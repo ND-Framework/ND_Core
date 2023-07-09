24caa1a8573359830e0d65fb7d284918f290c6f4
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2023-07-09 03:38:17 +0200

    feat: get all players & by metadata

--- server/main.lua
+++ server/main.lua
@@ -4,6 +4,17 @@ function NDCore.getPlayer(src)
     return ActivePlayers[src]
 end
 
+function NDCore.getPlayers(metadata, data)
+    if not metadata or not data then return ActivePlayers end 
+    local players = {}
+    for src, info in pairs(ActivePlayers) do
+        if info.metadata[metadata] == data then
+            players[src] = info
+        end
+    end
+    return players
+end
+
 AddEventHandler("playerDropped", function()
     local src = source
     local char = ActivePlayers[src]
