1ab7765cff71a864eeaf637a2bc86c1f3af5ce2a
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2023-07-13 01:22:24 +0200

    update: events and checks

--- client/events.lua
+++ client/events.lua
@@ -1,32 +1,34 @@
-RegisterNetEvent("ND:returnCharacters", function(characters)
-    NDCore.Characters = characters
-end)
-
 -- updates the money on the client.
 RegisterNetEvent("ND:updateMoney", function(cash, bank)
+    if not NDCore.player then return end
     NDCore.player.cash = cash
     NDCore.player.bank = bank
 end)
 
+local function removeCharacterFunctions(character)
+    local newData = {}
+    for k, v in pairs(character) do
+        if type(v) ~= "function" then
+            newData[k] = v
+        end
+    end
+    return newData
+end
+
 -- Sets main character.
-RegisterNetEvent("ND:setCharacter", function(character)
-    NDCore.player = character
+RegisterNetEvent("ND:characterLoaded", function(character)
+    if not NDCore.player then return end
+    NDCore.player = removeCharacterFunctions(character)
 end)
 
 -- Update main character info.
 RegisterNetEvent("ND:updateCharacter", function(character)
-    NDCore.player = character
+    if not NDCore.player then return end
+    NDCore.player = removeCharacterFunctions(character)
 end)
 
 -- Updates last lcoation.
 RegisterNetEvent("ND:updateLastLocation", function(location)
     if not NDCore.player then return end
     NDCore.player.lastLocation = location
-end)
-
--- Enable pvp for players.
-AddEventHandler("playerSpawned", function()
-    print("^0ND Framework support discord: ^5https://discord.gg/Z9Mxu72zZ6")
-    SetCanAttackFriendly(PlayerPedId(), true, false)
-    NetworkSetFriendlyFireOption(true)
 end)
