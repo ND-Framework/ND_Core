a2ad04106a19e89272456dcb9d038958521d8c09
Willow-6855 <104696802+Willow-6855@users.noreply.github.com>
2023-08-06 07:46:15 -0400

    Add files via upload

--- config_server.lua
+++ config_server.lua
@@ -1,6 +1,6 @@
 -- For support join my discord: https://discord.gg/Z9Mxu72zZ6
 
 server_config = {
-    discordServerToken = "OTAwODg1MDMxMDQ2Nzc0ODQ1.GU1TSC.Ed8D2_wcU-UQ1vZhe1khahgV8J3hLahOp4N1D8", -- discord bot token for permissions
-    guildId = "872496972454592523", -- discord guild id
+    discordServerToken = "REPLACE ME", -- discord bot token for permissions
+    guildId = "REPLACE ME", -- discord guild id
 }
