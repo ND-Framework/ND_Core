0848480753dfdc7d308e4203fe08d6c241e3cc8f
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2023-09-26 03:16:02 +0200

    refactor(database/vehicles): vehicle availability

--- database/vehicles.sql
+++ database/vehicles.sql
@@ -5,7 +5,9 @@ CREATE TABLE IF NOT EXISTS `nd_vehicles` (
 	`glovebox` LONGTEXT DEFAULT '[]',
 	`trunk` LONGTEXT DEFAULT '[]',
 	`properties` LONGTEXT DEFAULT '[]',
-	`stored` INT(11) DEFAULT NULL,
+    `stored` INT(11) DEFAULT '1',
+	`impounded` INT(11) DEFAULT '0',
+	`stolen` INT(11) DEFAULT '0',
 	PRIMARY KEY (`id`) USING BTREE,
 	INDEX `owner` (`owner`) USING BTREE,
 	CONSTRAINT `vehowner` FOREIGN KEY (`owner`) REFERENCES `nd_characters` (`charid`) ON UPDATE CASCADE ON DELETE CASCADE
